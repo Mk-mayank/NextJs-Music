4e1cac112581ae6b4ac094bd9909b3c86da52499
MayankMK <mayankmk1545@gmail.com>
2024-07-09 18:21:33 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+        domains: ['tse2.mm.bing.net', 'tse1.mm.bing.net', 'example.com'],  // Add any other domains where you get images
+    },
+    // Other Next.js configurations can go here
+  };
+  
+  export default nextConfig;
+  
